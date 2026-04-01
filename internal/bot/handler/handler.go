5d4b979ec60eaa8c8bf8f1c2a4dd5deb6b2c694e
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-04-01 14:09:35 +0700

    [db] unify errors checking

--- internal/bot/handler/handler.go
+++ internal/bot/handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
-import "github.com/go-core-fx/telegofx"
+import (
+	"github.com/go-core-fx/telegofx"
+)
 
 // Handler registers bot routes in the shared telegofx router.
 type Handler interface {
